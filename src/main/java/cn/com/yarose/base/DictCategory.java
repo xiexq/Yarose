de0c79b9bc78823c46c7ad1d8c708f2f394c6570
xxq <xxq@edu-edu.com.cn>
2015-08-19 21:27:01 +0800

    修改 setbean

--- src/main/java/cn/com/yarose/base/DictCategory.java
+++ src/main/java/cn/com/yarose/base/DictCategory.java
@@ -2,26 +2,25 @@ package cn.com.yarose.base;
 
 import cn.com.eduedu.jee.entity.annotation.FieldMeta;
 
-public class DictCategory implements java.io.Serializable{
+public class DictCategory implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
-	@FieldMeta(id=true,label="ID",editable=false)
+	@FieldMeta(id = true, label = "ID", editable = false)
 	Integer id;
-	
+
 	private String name;
 
-	public DictCategory(Integer id, String name) {
+	public Integer getId() {
+		return id;
+	}
+
+	public DictCategory(String name) {
 		super();
-		this.id = id;
 		this.name = name;
 	}
 
-	public Integer getId(){
-		return id;
-	}
-	
-	public void setId(Integer id){
-		this.id=id;
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getName() {
