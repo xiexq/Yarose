bac0b139f4526cd7b2da137e4d1b94f7784015b9
Administrator <Administrator@192.168.10.14>
2015-08-21 03:53:17 +0800

    优化代码

---
+++ src/main/java/cn/com/eduedu/jee/security/account/AccountService.java
@@ -0,0 +1,17 @@
+package cn.com.eduedu.jee.security.account;
+
+import java.util.List;
+
+import org.springframework.security.authentication.encoding.PasswordEncoder;
+
+import cn.com.eduedu.jee.service.BaseSearchService;
+
+public abstract interface AccountService extends
+		BaseSearchService<Account, Long> {
+	public boolean checkExistAccountByUserID(String paramString);
+
+	public PasswordEncoder getPasswordEncoder();
+
+	public List<Account> findByUserId(String userId, int offset, int count);
+
+}
