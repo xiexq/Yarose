c04abd2faa4b9b4c9582fb925fc3a79727ddcbe8
xxq <xxq@edu-edu.com.cn>
2015-08-23 15:23:29 +0800

    添加memberCardController

---
+++ src/main/java/cn/com/yarose/web/controller/home/shopManager/MemberCardController.java
@@ -0,0 +1,17 @@
+package cn.com.yarose.web.controller.home.shopManager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.com.eduedu.jee.mvc.controller.CRUDControllerMeta;
+import cn.com.yarose.card.MemberCard;
+import cn.com.yarose.card.MemberCardService;
+import cn.com.yarose.web.controller.BaseCRUDControllerExt;
+
+@Controller
+@RequestMapping("/home/shopmanager/membercard")
+@CRUDControllerMeta(title = "会员卡管理", service = MemberCardService.class, listable = true,countable=true,searchable=true)
+public class MemberCardController extends BaseCRUDControllerExt<MemberCard, Long> {
+
+
+}
