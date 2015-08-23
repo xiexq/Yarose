03c2ee634c71b2f39801c221869b8a3d1a35b31b
xxq <xxq@edu-edu.com.cn>
2015-08-23 15:16:46 +0800

    添加包

---
+++ src/main/java/cn/com/yarose/card/MemberCardService.java
@@ -0,0 +1,7 @@
+package cn.com.yarose.card;
+
+import cn.com.eduedu.jee.service.BaseSearchService;
+
+public interface MemberCardService extends BaseSearchService<MemberCard, Long> {
+
+}
---
+++ src/main/java/cn/com/yarose/card/dao/MemberCardDaoHibernateImpl.java
@@ -0,0 +1,8 @@
+package cn.com.yarose.card.dao;
+
+import cn.com.eduedu.jee.db.orm.hibernate.HibernateBaseDaoImpl;
+import cn.com.yarose.card.MemberCard;
+
+public class MemberCardDaoHibernateImpl extends HibernateBaseDaoImpl<MemberCard, Long>
+		implements MemberCardDao {
+}
